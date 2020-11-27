327d5b0aeca949c4983ccf520ed7e9564696c307
Kerry Buckley <kerryjbuckley@gmail.com>
2020-11-27 16:52:47 +0000

    New project

---
+++ lib/track_and_trace.ex
@@ -0,0 +1,18 @@
+defmodule TrackAndTrace do
+  @moduledoc """
+  Documentation for `TrackAndTrace`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TrackAndTrace.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/track_and_trace_test.exs
@@ -0,0 +1,8 @@
+defmodule TrackAndTraceTest do
+  use ExUnit.Case
+  doctest TrackAndTrace
+
+  test "greets the world" do
+    assert TrackAndTrace.hello() == :world
+  end
+end
