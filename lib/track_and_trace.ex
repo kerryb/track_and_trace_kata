e122a5ccbec055c95e9bcc0e9ee6c1af5ba2e47c
Kerry Buckley <kerryjbuckley@gmail.com>
2020-11-27 17:00:02 +0000

    Remove generated boilerplate

--- lib/track_and_trace.ex
+++ lib/track_and_trace.ex
@@ -1,18 +1,2 @@
 defmodule TrackAndTrace do
-  @moduledoc """
-  Documentation for `TrackAndTrace`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TrackAndTrace.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/track_and_trace_test.exs
+++ test/track_and_trace_test.exs
@@ -1,8 +1,4 @@
 defmodule TrackAndTraceTest do
   use ExUnit.Case
   doctest TrackAndTrace
-
-  test "greets the world" do
-    assert TrackAndTrace.hello() == :world
-  end
 end
