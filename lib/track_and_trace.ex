767818ba6d0ba86aaa542045814f5b559c54e024
Kerry Buckley <kerryjbuckley@gmail.com>
2020-11-29 13:32:12 +0000

    Add build_graph/1 function

--- lib/track_and_trace.ex
+++ lib/track_and_trace.ex
@@ -17,4 +17,34 @@ defmodule TrackAndTrace do
   def process(_people, _patient_zero) do
     []
   end
+
+  # This annotation excludes the function from the module's documented public API
+  @doc false
+  def build_graph(data) do
+    Enum.reduce(data, %{}, &add_person_to_graph/2)
+  end
+
+  defp add_person_to_graph(person, graph) do
+    graph
+    |> map_name_to_places(person)
+    |> map_places_to_name(person)
+  end
+
+  defp map_name_to_places(graph, person) do
+    Map.put(graph, {:name, person.name}, new_place_set(person.check_ins))
+  end
+
+  defp new_place_set(check_ins) do
+    check_ins
+    |> Enum.map(&{:place, &1})
+    |> MapSet.new()
+  end
+
+  defp map_places_to_name graph, person do
+    Enum.reduce(person.check_ins, graph, &add_place_to_graph(&1, person.name, &2))
+  end
+
+  defp add_place_to_graph(place, name, graph) do
+    Map.update(graph, {:place, place}, MapSet.new([{:name, name}]), &MapSet.put(&1, {:name, name}))
+  end
 end
--- test/track_and_trace_test.exs
+++ test/track_and_trace_test.exs
@@ -1,4 +1,21 @@
 defmodule TrackAndTraceTest do
   use ExUnit.Case
   doctest TrackAndTrace
+
+  describe "TrackAndTrace/build_graph/1" do
+    test "returns a map of nodes (names/places) to their directly-connected nodes" do
+      assert TrackAndTrace.build_graph([
+               %{name: "Alice", check_ins: [1, 2, 3]},
+               %{name: "Bob", check_ins: [2, 4]}
+             ]) ==
+               %{
+                 {:name, "Alice"} => MapSet.new([{:place, 1}, {:place, 2}, {:place, 3}]),
+                 {:name, "Bob"} => MapSet.new([{:place, 2}, {:place, 4}]),
+                 {:place, 1} => MapSet.new([{:name, "Alice"}]),
+                 {:place, 2} => MapSet.new([{:name, "Alice"}, {:name, "Bob"}]),
+                 {:place, 3} => MapSet.new([{:name, "Alice"}]),
+                 {:place, 4} => MapSet.new([{:name, "Bob"}])
+               }
+    end
+  end
 end
